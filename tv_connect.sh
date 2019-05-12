f04f4ab096414a8da6d7ca8600b5a00d90514b12
JustinSchwarzwald <43459254+JustinSchwarzwald@users.noreply.github.com>
2019-05-12 04:42:46 +0000

    Add files via upload

---
+++ tv_connect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --output LVDS-1 --mode 1366x768 --scale 1x1 --output HDMI1 --same-as LVDS-1 --mode 1920x1080 --scale 0.711x0.711
+xrandr --output LVDS-1 --mode 1366x768 --scale 1x1 --output HDMI0 --same-as LVDS-1 --mode 1920x1080 --scale 0.711x0.711
